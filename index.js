2e92f8dc1778deaff2c8bbfca0019fb957cb79e6
zhangwenshun <378253487@qq.com>
2018-10-23 16:13:33 +0800

    优化项目结构

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-import React from 'react'
 import { AppRegistry } from 'react-native'
-import Component from './src/components/base'
-import RootStack from './src/route'
-
-class QOSWalletExample extends Component {
-  render() {
-    return <RootStack />;
-  }
-}
+import QOSWalletExample from './src/main'
 
 // 注册组件
 AppRegistry.registerComponent('qosexample', () => QOSWalletExample);
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Component from './components/base'
+import RootStack from './route'
+
+export default class QOSWalletExample extends Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
