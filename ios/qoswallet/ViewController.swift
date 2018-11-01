79ccc8f4fbfb4bd54a29c8d41ae572941f0d8a59
wanghao <skydevin@qq.com>
2018-11-01 17:04:53 +0800

    添加指纹登录功能

--- ios/qoswallet/ViewController.swift
+++ ios/qoswallet/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Alamofire
 import SSZipArchive
+import LocalAuthentication
 
 class ViewController: UIViewController {
     
@@ -38,14 +39,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        #if DEBUG
-//        loadLocalModuleConfigure()
-//        print("DEBUG模式")
-//        #else
-//        requestConfigure()
-//        print("Release模式")
-//        #endif
-    
+        addTouchId()
         ReactRootViewManager.shared.preLoadRootViewWithName(viewName: "QOSWallet")
         view = ReactRootViewManager.shared.rootViewWithName(viewName: "QOSWallet")
     }
@@ -111,6 +105,35 @@ class ViewController: UIViewController {
             loadRNRootView()
         } else {
             downloadJSBundle()
+        }
+    }
+    
+    func addTouchId() {
+        //1.初始化TouchID句柄
+        let authentication = LAContext()
+        var error: NSError?
+        //2.检查Touch ID是否可用
+        let isAvailable = authentication.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
+                                                           error: &error)
+        //3.处理结果
+        if isAvailable {
+            NSLog("Touch ID is available")
+            //这里是采用认证策略 LAPolicy.DeviceOwnerAuthenticationWithBiometrics
+            //--> 指纹生物识别方式
+            authentication.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "这里需要您的指纹来进行识别验证", reply: {
+                //当调用authentication.evaluatePolicy方法后，系统会弹提示框提示用户授权
+                (success, error) -> Void in
+                if success {
+                    NSLog("您通过了Touch ID指纹验证！")
+                }
+                else {
+                    //上面提到的指纹识别错误
+                    NSLog("您未能通过Touch ID指纹验证！错误原因：\n\(String(describing: error))")
+                }
+            })
+        } else{
+            //上面提到的硬件配置
+            NSLog("Touch ID不能使用！错误原因：\n\(String(describing: error))")
         }
     }
 }
