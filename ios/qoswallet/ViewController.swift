b4eafd09f91aa3484032dbb30fceae0e19581a2c
wanghao <skydevin@qq.com>
2018-11-02 20:30:09 +0800

    优化代码

--- ios/qoswallet/ViewController.swift
+++ ios/qoswallet/ViewController.swift
@@ -140,38 +140,6 @@ class ViewController: UIViewController {
 
 extension ViewController {
     
-    func addTouchId() {
-        //1.初始化TouchID句柄
-        let authentication = LAContext()
-        var error: NSError?
-        //2.检查Touch ID是否可用
-        let isAvailable = authentication.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
-                                                           error: &error)
-        //3.处理结果
-        if isAvailable {
-            NSLog("Touch ID is available")
-            //这里是采用认证策略 LAPolicy.DeviceOwnerAuthenticationWithBiometrics
-            //--> 指纹生物识别方式
-            authentication.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "这里需要您的指纹来进行识别验证", reply: {
-                //当调用authentication.evaluatePolicy方法后，系统会弹提示框提示用户授权
-                (success, error) -> Void in
-                if success {
-                    NSLog("您通过了Touch ID指纹验证！")
-                }
-                else {
-                    //上面提到的指纹识别错误
-                    NSLog("您未能通过Touch ID指纹验证！错误原因：\n\(String(describing: error))")
-                }
-            })
-        } else{
-            //上面提到的硬件配置
-            NSLog("Touch ID不能使用！错误原因：\n\(String(describing: error))")
-        }
-    }
-}
-
-extension ViewController {
-    
     //加载本地Module配置文件
     func loadLocalModuleConfigure() {
         let plistPath = Bundle.main.path(forResource: "localModuleConfig", ofType: "plist")
