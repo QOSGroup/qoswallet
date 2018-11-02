09d0b84aed4c053ce5f238cdcc4cffbe6be5b95e
wanghao <skydevin@qq.com>
2018-11-02 19:46:50 +0800

    添加安卓工程

---
+++ android/app/src/main/java/cn/tokenxy/qos/qoswallet/QosActivity.java
@@ -0,0 +1,46 @@
+package cn.tokenxy.qos.qoswallet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class QosActivity extends AppCompatActivity {
+
+    private static final int OVERLAY_PERMISSION_REQ_CODE = 0;
+//    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qos);
+
+//        btn = findViewById(R.id.btn);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            }
+        }
+
+        Intent intent = new Intent(QosActivity.this, QosReactActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    // SYSTEM_ALERT_WINDOW permission not granted...
+                }
+            }
+        }
+    }
+}
